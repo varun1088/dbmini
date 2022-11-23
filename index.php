c4c5042c51b1a4c01590bc89df81cc33a8d9d1af
varun1088 <108451137+varun1088@users.noreply.github.com>
2022-11-23 15:31:48 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,344 @@
+<!doctype html>
+<html lang="en">
+  <head>
+   
+    <title>Sanitization Management System || Home Page</title>
+    
+    <link href="https://fonts.googleapis.com/css2?family=Kumbh+Sans:wght@300;400;700&display=swap" rel="stylesheet">
+
+    <!-- Template CSS -->
+    <link rel="stylesheet" href="assets/css/style-starter.css">
+  </head>
+  <body>
+<?php include_once('includes/topbar.php');?>
+<!-- main-slider -->
+<section class="w3l-main-slider" id="home">
+    <div class="companies20-content">
+        <div class="owl-one owl-carousel owl-theme">
+            <div class="item">
+                <li>
+                    <div class="slider-info banner-view bg bg2">
+                        <div class="banner-info">
+                            <div class="container">
+                                <div class="banner-info-bg">
+                                    <h5>Disinfecting & Deodorizing <span>Spray services.</span> </h5>
+                                    <p class="mt-md-4 mt-3">Imagine your home so clean, it’s clinically clean. </p>
+                                    <a class="btn btn-style btn-primary mt-sm-5 mt-4 mr-2" href="about.php"> Learn
+                                        More</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </li>
+            </div>
+            <div class="item">
+                <li>
+                    <div class="slider-info  banner-view banner-top1 bg bg2">
+                        <div class="banner-info">
+                            <div class="container">
+                                <div class="banner-info-bg">
+                                    <h5>Residential and Commercial Sanitizing <span> services</span>  </h5>
+                                    <p class="mt-md-4 mt-3"> Free from bacteria and viruses, free from mold and mildew. </p>
+                                    <a class="btn btn-style btn-primary mt-sm-5 mt-4 mr-2" href="about.php"> Learn
+                                        More</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </li>
+            </div>
+            <div class="item">
+                <li>
+                    <div class="slider-info banner-view banner-top2 bg bg2">
+                        <div class="banner-info">
+                            <div class="container">
+                                <div class="banner-info-bg">
+                                    <h5>Disinfecting & Deodorizing <span>Spray services.</span> </h5>
+                                    <p class="mt-md-4 mt-3">Imagine your home so clean, it’s clinically clean. </p>
+                                    <a class="btn btn-style btn-primary mt-sm-5 mt-4 mr-2" href="about.php"> Learn
+                                        More</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </li>
+            </div>
+            <div class="item">
+                <li>
+                    <div class="slider-info banner-view banner-top3 bg bg2">
+                        <div class="banner-info">
+                            <div class="container">
+                                <div class="banner-info-bg">
+                                    <h5>Residential and Commercial Sanitizing <span> services</span>  </h5>
+                                    <p class="mt-md-4 mt-3"> Free from bacteria and viruses, free from mold and mildew. </p>
+                                    <a class="btn btn-style btn-primary mt-sm-5 mt-4 mr-2" href="about.php"> Learn
+                                        More</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </li>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- /main-slider -->
+<!-- our products -->
+<section class="w3l-products py-5" id="services">
+    <div class="container py-lg-5 py-md-4 py-2">
+        <div class="row">
+            <div class="col-lg-12 mx-auto">
+                <!--Horizontal Tab-->
+                <div id="parentHorizontalTab">
+                    <ul class="resp-tabs-list hor_1">
+                        <li>Residential</li>
+                        <li>Commercial</li>
+                        <div class="clear"></div>
+                    </ul>
+                    <div class="resp-tabs-container hor_1 mt-md-0 mt-4">
+                        <div class="products-content">
+                            <div class="row">
+                                <div class="col-lg-6">
+                                    <h3>Residential sanitizing</h3>
+                                    <p>Imagine your home so clean, it’s clinically clean. 
+                                        Lorem ipsum dolor sit, elit dolores nulla quisdam. minus aliquam corrupti, 
+                                        modi ipsum natus sit pariatur. Curabitur orci sed leo mattis, nec maximus 
+                                        nibh faucibus lorem dolor sit. </p>
+                                    <div class="grids-ser my-md-5 my-4 text-center">
+                                        <div class="area-box">
+                                            <span class="fa fa-bed"></span>
+                                            <h4><a href="#feature" class="title-head">Mattress </a></h4>
+                                        </div>
+                                        <div class="area-box">
+                                            <span class="fa fa-glass"></span>
+                                            <h4><a href="#feature" class="title-head">Furniture </a></h4>
+                                        </div>
+                                        <div class="area-box">
+                                            <span class="fa fa-futbol-o"></span>
+                                            <h4><a href="#feature" class="title-head">Carpet </a></h4>
+                                        </div>
+                                    </div>
+                                    <a href="services.php" class="btn btn-outline-primary btn-style mt-3">Get Service Now
+                                        <span class="fa fa-chevron-right"></span>
+                                    </a>
+                                </div>
+                                <div class="col-lg-6 mt-lg-0 mt-5">
+                                    <img src="assets/images/residentsan.jpg" class="img-fluid radius-image-full"
+                                        alt="project image">
+                                </div>
+                            </div>
+                        </div>
+                        <div class="products-content">
+                            <div class="row">
+                                <div class="col-lg-6">
+                                    <h3>Commercial sanitizing</h3>
+                                    <p>Imagine your home so clean, it’s clinically clean. 
+                                        Lorem ipsum dolor sit, elit dolores nulla quisdam. minus aliquam corrupti, 
+                                        modi ipsum natus sit pariatur. Curabitur orci sed leo mattis, nec maximus 
+                                        nibh faucibus lorem dolor sit. </p>
+                                        <div class="grids-ser my-md-5 my-4 text-center">
+                                            <div class="area-box">
+                                                <span class="fa fa-glass"></span>
+                                                <h4><a href="#feature" class="title-head">Factory </a></h4>
+                                            </div>
+                                            <div class="area-box">
+                                                <span class="fa fa-bed"></span>
+                                                <h4><a href="#feature" class="title-head">Building </a></h4>
+                                            </div>
+                                            <div class="area-box">
+                                                <span class="fa fa-futbol-o"></span>
+                                                <h4><a href="#feature" class="title-head">Education </a></h4>
+                                            </div>
+                                        </div>
+                                    <a href="services.php" class="btn btn-outline-primary btn-style mt-3">Get Service Now
+                                        <span class="fa fa-chevron-right"></span>
+                                    </a>
+                                </div>
+                                <div class="col-lg-6 mt-lg-0 mt-5">
+                                    <img src="assets/images/commercialsan.jpg" class="img-fluid radius-image-full"
+                                        alt="project image">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- //our products -->
+<!-- home block3 -->
+<section class="wthree-row py-5 about-main" id="">
+    <div class="container py-lg-5 py-md-4 py-2">
+        <div class="row">
+            <div class="col-lg-6 mb-lg-0 mb-sm-5 mb-4">
+                <h3 class="title-small mb-2">Protecting Our Environment</h3>
+                <h3 class="title-big">Scientifically Proven Effective Against </h3>
+                <p class="pt-4">Aliquam ac est vel nisl init et justo vel ut nibh rhoncusm vel eget
+                    enim. Curabitur mattis orci sed leo mattis, nec maximus nibh faucibus lorem dolor sit. Aliquam ac
+                    est vel nisl init et justo vel ut nibh rhoncusm vel eget enim. Curabitur mattis orci.</p>
+                <a href="about.php" class="btn btn-style btn-primary mt-lg-5 mt-4">Know More</a>
+            </div>
+            <div class="col-lg-6 text-center">
+                <div class="agileits-banner-info4">
+                    <img src="assets/images/image1.jpg" alt="image" class="img-fluid radius-image" />
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- //home block3 -->
+
+
+
+
+
+
+<!-- footer -->
+<?php include_once('includes/footer.php');?>
+
+<!--  javascripts file here -->
+<script src="assets/js/jquery-3.3.1.min.js"></script>
+<script src="assets/js/jquery-1.9.1.min.js"></script>
+
+<script src="assets/js/theme-change.js"></script> <!-- //light and dark mode switch js -->
+<!-- responsive tabs -->
+<script src="assets/js/easyResponsiveTabs.js"></script>
+
+<!--Plug-in Initialisation-->
+<script type="text/javascript">
+  $(document).ready(function () {
+    //Horizontal Tab
+    $('#parentHorizontalTab').easyResponsiveTabs({
+      type: 'default', //Types: default, vertical, accordion
+      width: 'auto', //auto or any width like 600px
+      fit: true, // 100% fit in a container
+      tabidentify: 'hor_1', // The tab groups identifier
+      activate: function (event) { // Callback function if tab is switched
+        var $tab = $(this);
+        var $info = $('#nested-tabInfo');
+        var $name = $('span', $info);
+        $name.text($tab.text());
+        $info.show();
+      }
+    });
+  });
+</script>
+<script type="text/javascript">
+  $(document).ready(function () {
+    //Horizontal Tab
+    $('#parentHorizontalTab1').easyResponsiveTabs({
+      type: 'default', //Types: default, vertical, accordion
+      width: 'auto', //auto or any width like 600px
+      fit: true, // 100% fit in a container
+      tabidentify: 'hor_2', // The tab groups identifier
+      activate: function (event) { // Callback function if tab is switched
+        var $tab = $(this);
+        var $info = $('#nested-tabInfo');
+        var $name = $('span', $info);
+        $name.text($tab.text());
+        $info.show();
+      }
+    });
+  });
+</script>
+
+<!-- owl carousel -->
+<script src="assets/js/owl.carousel.js"></script>
+<!-- script for banner slider-->
+<script>
+  $(document).ready(function () {
+    $('.owl-one').owlCarousel({
+      loop: true,
+      margin: 0,
+      nav: false,
+      responsiveClass: true,
+      autoplay: true,
+      autoplayTimeout: 5000,
+      autoplaySpeed: 1000,
+      autoplayHoverPause: false,
+      responsive: {
+        0: {
+          items: 1
+        },
+        480: {
+          items: 1
+        },
+        667: {
+          items: 1
+        },
+        1000: {
+          items: 1
+        }
+      }
+    })
+  })
+</script>
+<!-- //script -->
+<!-- owl carousel -->
+<!-- script for tesimonials carousel slider -->
+<script>
+  $(document).ready(function () {
+    $("#owl-demo1").owlCarousel({
+      loop: true,
+      nav: false,
+      responsiveClass: true,
+      responsive: {
+        0: {
+          items: 1,
+          nav: false
+        },
+        736: {
+          items: 1,
+          nav: false
+        }
+      }
+    })
+  })
+</script>
+<!-- //script for tesimonials carousel slider -->
+
+<!-- disable body scroll which navbar is in active -->
+<script>
+  $(function () {
+    $('.navbar-toggler').click(function () {
+      $('body').toggleClass('noscroll');
+    })
+  });
+</script>
+<!-- disable body scroll which navbar is in active -->
+
+<!--/MENU-JS-->
+<script>
+  $(window).on("scroll", function () {
+    var scroll = $(window).scrollTop();
+
+    if (scroll >= 80) {
+      $("#site-header").addClass("nav-fixed");
+    } else {
+      $("#site-header").removeClass("nav-fixed");
+    }
+  });
+
+  //Main navigation Active Class Add Remove
+  $(".navbar-toggler").on("click", function () {
+    $("header").toggleClass("active");
+  });
+  $(document).on("ready", function () {
+    if ($(window).width() > 991) {
+      $("header").removeClass("active");
+    }
+    $(window).on("resize", function () {
+      if ($(window).width() > 991) {
+        $("header").removeClass("active");
+      }
+    });
+  });
+</script>
+<!--//MENU-JS-->
+
+<script src="assets/js/bootstrap.min.js"></script><!-- //bootstrap js -->
+
+</body>
+
+</html>
